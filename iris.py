264b304c6d24a29726d2a7f01605840938b83621
A00466845 <rashad.ahmed.imtiaz@smu.ca>
2023-02-15 19:58:51 -0400

    updation

--- iris.py
+++ iris.py
@@ -2,6 +2,7 @@ from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import tree
+from joblib import dump
 
 data = load_iris()
 
@@ -28,3 +29,6 @@ train_accuracy= accuracy_score(train_y, pred_y_train)
 print(train_accuracy)
 
 print(data['target_names'][pred_y])
+
+
+dump(clf,"DT.joblib")
